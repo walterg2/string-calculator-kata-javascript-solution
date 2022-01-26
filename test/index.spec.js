d329df350415803fe71a07bd792addbf5bfbc96a
George Walters <walterg2@gmail.com>
2022-01-26 09:12:08 -0500

    Adding the ability to return the number passed in when only one is passed

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,7 +5,12 @@ describe('add()', () => {
     before(function() {
         this.subject = new StringCalculator()
     })
+
     it('can take an empty string and return 0', function() {
         expect(this.subject.add('')).to.eq(0)
+    })
+
+    it('can take a single number and return that number', function () {
+        expect(this.subject.add('3')).to.eq(3)
     })
 })
